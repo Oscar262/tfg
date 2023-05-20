16884cbb18b13502e5e5dbe3f2e16f636fb2d9da
OscarSanchez <os.sanav@gmail.com>
2023-05-20 12:57:21 +0200

    correct user add

--- src/main/java/org/iesfm/app/service/UserService.java
+++ src/main/java/org/iesfm/app/service/UserService.java
@@ -141,7 +141,7 @@ public class UserService {
             String emailUser = entity.getEmail() + "1";
 
             entity.setEmail(emailUser);
-            if (userDao.findByEmail(entity.getEmail() + email) != null){
+            if (userDao.findByEmailOnlyName(entity.getEmail() + email) != null){
                 return checkEmail(entity, emailUser);
             }else return emailUser + email;
         }
